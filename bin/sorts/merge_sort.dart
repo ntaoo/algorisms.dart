19e203a08fe12000740a77c85959f3aa91d5dd24
ntaoo <ntaoo.g@gmail.com>
2013-07-09 15:54:02 +0900

    add merge_sort

---
+++ bin/sorts/merge_sort.dart
@@ -0,0 +1,68 @@
+import 'dart:math';
+
+// Using fixed-length List
+void mergeSort(List<dynamic> list) {
+  if (list.length <= 1) return;
+
+  void mergeBySort(int startIndexL, int endIndexL, int startIndexH, int endIndexH) {
+    List<dynamic> merge = new List((endIndexL - startIndexL + 1) + (endIndexH - startIndexH + 1));
+    int mergeIndex = 0;
+    int indexL = startIndexL;
+    int indexH = startIndexH;
+
+    while (indexL <= endIndexL && indexH <= endIndexH) {
+      if (list[indexL] < list[indexH]) {
+        merge[mergeIndex] = list[indexL];
+        mergeIndex++;
+        indexL++;
+      } else {
+        merge[mergeIndex] = list[indexH];
+        mergeIndex++;
+        indexH++;
+      }
+    }
+
+    if (indexL > endIndexL) {
+      for (indexH; indexH <= endIndexH; indexH++) {
+        merge[mergeIndex] = list[indexH];
+        mergeIndex++;
+      }
+    } else {
+      for (indexL; indexL <= endIndexL; indexL++) {
+        merge[mergeIndex] = list[indexL];
+        mergeIndex++;
+      }
+    }
+
+    // replace sub list with merged
+    mergeIndex = 0;
+    for (int i = startIndexL; i <= endIndexH; i++) {
+      list[i] = merge[mergeIndex];
+      mergeIndex++;
+    }
+  }
+
+  void sort(int startIndex, int endIndex) {
+    if (startIndex < endIndex) {
+      int middleIndex = startIndex + ((endIndex - startIndex) / 2).toInt();
+      sort(startIndex, middleIndex);
+      sort(middleIndex + 1, endIndex);
+      mergeBySort(startIndex, middleIndex, middleIndex + 1, endIndex);
+    }
+  }
+
+  sort(0, list.length - 1);
+}
+
+main() {
+  const maxNumber = 100000;
+  const numbersLength = 10;
+  var random = new Random();
+
+  var numbers = new List<int>(numbersLength);
+  for (int i = 0; i < numbersLength; i++) numbers[i] = random.nextInt(maxNumber);
+
+  print("Before Sort : $numbers");
+  mergeSort(numbers);
+  print("After Sort :  $numbers");
+}
